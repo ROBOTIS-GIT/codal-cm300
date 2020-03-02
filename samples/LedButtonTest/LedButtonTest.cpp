573c3dae2c363e98bddd13070d981b7c539a5f39
Kei <kkw@robotis.com>
2020-03-02 12:18:19 +0900

    Modified sample codes for baremetal (not RTOS).

--- samples/LedButtonTest/LedButtonTest.cpp
+++ samples/LedButtonTest/LedButtonTest.cpp
@@ -1,43 +1,28 @@
 #include "CM300.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "timers.h"
-
-static void button_led_task_function(void * pvParameter);
-
 CM300 cm;
+
+/* Sample global code START */
+  // Headers for sample code
+  // Global Class instances
+  // Global variables
+  // Global etc.
+/* Sample global code END */
 
 int main()
 {
   cm.init();
 
-  /* Create task for Button-LED with priority set to 2 */
-  xTaskCreate(button_led_task_function, "BUTTON-LED", configMINIMAL_STACK_SIZE + 200, NULL, 2, NULL);
-
-  /* Start FreeRTOS scheduler. */
-  vTaskStartScheduler();
+  /* Sample init code START */
+  bool led_state = false;
+  cm.io.led.setDigitalValue(led_state);
+  /* Sample init code END */
 
   while(1)
   {
-  }
-}
-
-/**@brief Button-LED task entry function.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the task.
- */
-static void button_led_task_function(void * pvParameter)
-{
-  (void)pvParameter;
-  bool led_state;
-  
-  while(true){
+    /* Sample loop code START */
     led_state = cm.io.start_button.getDigitalValue();
-    cm.io.led.setDigitalValue(led_state);
-
-    /* Delay a task for a given number of ticks */
-    vTaskDelay(100);
-
-    /* Tasks must be implemented to never return... */
+    cm.io.led.setDigitalValue(led_state);   
+    cm.sleep(100);
+    /* Sample loop code END */
   }
 }
