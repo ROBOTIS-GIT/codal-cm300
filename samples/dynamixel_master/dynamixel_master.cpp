af9a78b9e652526009e11ac2cd369a05b0b38030
Kei <kkw@robotis.com>
2020-03-02 11:12:19 +0900

    Modified dynamixel_master for baremetal(not RTOS).

--- samples/dynamixel_master/dynamixel_master.cpp
+++ samples/dynamixel_master/dynamixel_master.cpp
@@ -1,61 +1,36 @@
 #include "CM300.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "timers.h"
-
-#include "CM300DYNAMIXEL.h"
-
-static void led_blink_task_function(void * pvParameter);
-static void dxl_master_task_function(void * pvParameter);
-
 CM300 cm;
+
+/* Sample global code START */
+#include "CM300DYNAMIXEL.h"
 CM300DYNAMIXEL dynamixel;
+/* Sample global code END */
 
 int main()
 {
   cm.init();
 
-  /* Create task for LED with priority set to 2 */
-  xTaskCreate(led_blink_task_function, "LED", configMINIMAL_STACK_SIZE + 200, NULL, 2, NULL);
-
-  /* Create task for DYNAMIXEL Master with priority set to 2 */
-  xTaskCreate(dxl_master_task_function, "DXL Master", 2*1024, NULL, 2, NULL);
-
-  /* Start FreeRTOS scheduler. */
-  vTaskStartScheduler();
+  /* Sample init code START */
+  uint8_t id[253];
+  uint8_t recv_cnt, i;
+  uint32_t pre_time = 0;
+  /* Sample init code END */
 
   while(1)
   {
-  }
-}
+    /* Sample loop code START */
+    if(cm.systemTime() - pre_time >= 500){
+      pre_time = cm.systemTime();
+      recv_cnt = dynamixel.ping(DXL_BROADCAST_ID, id, sizeof(id));
+      cm.serial.printf("Ping recv cnt: %d \r\n", recv_cnt);
+      for(i=0; i<recv_cnt; i++)
+      {
+        cm.serial.printf("\t ID: %d\r\n", id[i]);
+      }
+    }
+    /* Sample loop code END */
 
-
-static void dxl_master_task_function(void * pvParameter)
-{
-  (void)pvParameter;
-  uint8_t id;
-  
-  for(;;){
-    dynamixel.ping(1, &id, 1);
-    vTaskDelay(500);
-  }
-}
-
-
-/**@brief LED task entry function.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the task.
- */
-static void led_blink_task_function(void * pvParameter)
-{
-  (void)pvParameter;
-  
-  while(true){
-    cm.io.led.setDigitalValue(!cm.io.led.getDigitalValue());
-
-    /* Delay a task for a given number of ticks */
-    vTaskDelay(500);
-
-    /* Tasks must be implemented to never return... */
+    // Blink the LED to check operation.
+    cm.blinkLed(500);
   }
 }
