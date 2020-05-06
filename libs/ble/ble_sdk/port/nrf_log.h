c9354d602a4041df7534cb4b91f9df3e3bf83b1f
Kei <kkw@robotis.com>
2020-05-06 10:20:48 +0900

    Add ble library (derived from nrf SDK)

---
+++ libs/ble/ble_sdk/port/nrf_log.h
@@ -0,0 +1,26 @@
+/*
+ * nrf_log.h
+ *
+ *  Created on: 2020. 4. 30.
+ *      Author: Baram
+ */
+
+#ifndef SRC_AP_NRF_SDK_UTIL_NRF_LOG_H_
+#define SRC_AP_NRF_SDK_UTIL_NRF_LOG_H_
+
+#include "app_error.h"
+
+#define NRF_LOG_ERROR(...)
+#define NRF_LOG_WARNING(...)
+#define NRF_LOG_INFO(...)
+#define NRF_LOG_DEBUG(...)
+
+#define NRF_LOG_MODULE_REGISTER()
+
+#define NRF_LOG_INST_ERROR(p_inst,...)
+#define NRF_LOG_INST_WARNING(p_inst,...)
+#define NRF_LOG_INST_INFO(p_inst,...)
+#define NRF_LOG_INST_DEBUG(p_inst,...)
+
+
+#endif /* SRC_AP_NRF_SDK_UTIL_NRF_LOG_H_ */
---
+++ libs/ble/ble_sdk/port/sdk_config_port.h
@@ -0,0 +1,18 @@
+/*
+ * sdk_config_port.h
+ *
+ *  Created on: 2020. 5. 2.
+ *      Author: Baram
+ */
+
+#ifndef SRC_LIB_BLE_SDK_PORT_SDK_CONFIG_PORT_H_
+#define SRC_LIB_BLE_SDK_PORT_SDK_CONFIG_PORT_H_
+
+
+#ifdef _USE_HW_BLEUART_C
+#include "sdk_config_nus_c.h"
+#else
+#include "sdk_config_nus_p.h"
+#endif
+
+#endif /* SRC_LIB_BLE_SDK_PORT_SDK_CONFIG_PORT_H_ */
